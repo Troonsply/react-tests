020210e6125602c4f6bfb90aa2ab4e655a8fc441
troonsply <troonsply@gmail.com>
2024-05-31 15:28:05 +0300

    tests for input

---
+++ src/components/Input/Input.test.jsx
@@ -0,0 +1,49 @@
+import {render, screen, fireEvent} from '@testing-library/react';
+import {Input} from './Input';
+
+const placeholder = 'test placeholder';
+
+const value = 'test value';
+
+describe('Input', () => {
+    it('should render the input', () => {
+        render(<Input placeholder={placeholder}/>)
+        expect(screen.getByPlaceholderText(placeholder)).toBeInTheDocument()
+    })
+    it('should render the input with the correct type', () => {
+        render(<Input type="checkbox" placeholder={placeholder}/>)
+        expect(screen.getByRole('checkbox')).toBeInTheDocument();
+    })
+    it('should render the input with the correct class names', () => {
+        const {container} = render(<Input
+            type="checkbox"
+            placeholder={placeholder}
+            inputClassName={'inputTest'}
+            containerClassName={'containerTest'}
+        />)
+        expect(container.querySelector('.formControl.containerTest')).toBeInTheDocument();
+        expect(screen.getByPlaceholderText(placeholder)).toHaveClass('inputTest');
+        expect(screen.getByPlaceholderText(placeholder)).toHaveClass('input');
+    })
+    it('should render the input without label', () => {
+        render(<Input type="checkbox" placeholder={placeholder}/>)
+        expect(screen.queryByTestId('input-label')).not.toBeInTheDocument();
+    })
+    it('should render the input with correct label', () => {
+        const label = 'test label';
+        render(<Input type="checkbox" placeholder={placeholder} label={label}/>)
+        expect(screen.getByLabelText(label)).toBeInTheDocument();
+    })
+    it('should render the input with correct value', () => {
+        render(<Input placeholder={placeholder} value={value} />)
+        expect(screen.getByDisplayValue(value)).toBeInTheDocument();
+    })
+    it('should invoke the onChange callback', () => {
+        const onChange = jest.fn();
+        render(<Input  placeholder={placeholder} value={value} onChange={onChange}/>)
+        const element = screen.getByPlaceholderText(placeholder);
+        fireEvent.change(element, {target: {value: '123456'}})
+
+        expect(onChange).toHaveBeenCalledTimes(1)
+    })
+})
